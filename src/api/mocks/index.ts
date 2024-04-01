6cdf68a12e6a99f50b5f44a2446a82076564dd6e
Urias01 <uriasluis64@gmail.com>
2024-04-01 11:21:07 -0300

    mock to register and dashboard

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,9 +2,25 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
+import { registerREstaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mocks'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(
+  signInMock,
+  registerREstaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getPopularProductsMock,
+  getDailyRevenueInPeriodMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
